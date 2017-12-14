9542a89cc24f5c39d31e6181f7ef901e50c96757
Sam Henry <Sam@Sams-MacBook-Pro.local>
2017-12-14 13:36:17 -0700

    Project 2

--- Android/project 2/Milestone 3/BDGT/app/src/main/java/com/example/sam/bdgt/MainActivity.java
+++ Android/project 2/Milestone 3/BDGT/app/src/main/java/com/example/sam/bdgt/MainActivity.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -24,16 +26,19 @@ public class MainActivity extends AppCompatActivity {
     EditText foodAmount;
     String foodValue;
     TextView remainingFoodValue;
+    String foodResult;
 
     //rent
     EditText rentAmount;
     String rentValue;
     TextView remainingRentValue;
+    String rentResult;
 
     //transit
     EditText transAmount;
     String transValue;
     TextView remainingTransitValue;
+    String transResult;
 
     //add item
     EditText itemAmount;
@@ -46,32 +51,65 @@ public class MainActivity extends AppCompatActivity {
 
         incomeAmount = (EditText) findViewById(R.id.incomeEnter);
 
+
         foodAmount = (EditText) findViewById(R.id.foodBudget);
         remainingFoodValue = (TextView) findViewById(R.id.remainingFood);
+        foodAmount.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-//        foodAmount.addTextChangedListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence s, int start, int before, int count) {
-//                remainingFoodValue.setText(s);
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable s) {
-//
-//            }
-//        });
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                remainingFoodValue.setText(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         rentAmount = (EditText) findViewById(R.id.rentBudget);
         remainingRentValue = (TextView) findViewById(R.id.remainingRent);
+        rentAmount.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                remainingRentValue.setText(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         transAmount = (EditText) findViewById(R.id.transBudget);
         remainingTransitValue = (TextView) findViewById(R.id.remainingTransit);
+        transAmount.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                remainingTransitValue.setText(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        //circle
         ImageView bgCircle = (ImageView)findViewById(R.id.imageView);
 
         itemAmount = (EditText) findViewById(R.id.enterItemAmount);
@@ -84,54 +122,77 @@ public class MainActivity extends AppCompatActivity {
         bgCircle.setImageDrawable(roundedBitmapDrawable);
     }
 
-    public void getValues(View view){
-        //income amount
-        incomeValue = incomeAmount.getText().toString();
 
-        if(TextUtils.isEmpty(incomeValue)){
-            Context context =  getApplicationContext();
+    public void updateValues(View view){
+        incomeValue = incomeAmount.getText().toString();
+        //spinner
+        Spinner category = (Spinner) findViewById(R.id.spinner);
+        Integer location = category.getSelectedItemPosition();
+
+        int result;
+
+        if (TextUtils.isEmpty(incomeValue)) {
+            Context context = getApplicationContext();
             CharSequence icomeToast = "Please Enter Income!";
             int duration = Toast.LENGTH_SHORT;
 
             Toast toast = Toast.makeText(context, icomeToast, duration);
             toast.show();
         }else{
-            updateValues(view);
-            //foodBudget
-//            foodValue = foodAmount.getText().toString();
+            int foodInt;
+            foodValue = foodAmount.getText().toString();
+            if(TextUtils.isEmpty(foodValue)){
+                foodInt = 0;
+            }else{
+                foodInt = Integer.parseInt(foodValue);
+            }
 
-            //rent Budget
-//            rentValue = rentAmount.getText().toString();
+            int rentInt;
+            rentValue = rentAmount.getText().toString();
+            if(TextUtils.isEmpty(rentValue)){
+                rentInt = 0;
+            }else{
+                rentInt = Integer.parseInt(rentValue);
+            }
 
-            //transit budget
-//            transValue = transAmount.getText().toString();
+            int transInt;
+            transValue = transAmount.getText().toString();
+            if(TextUtils.isEmpty(transValue)){
+                transInt = 0;
+            }else{
+                transInt = Integer.parseInt(transValue);
+            }
 
-            //remaining food
-            //remainingFoodValue.setText(foodValue);
+            itemValue = itemAmount.getText().toString();
+            int itemInt;
+            if(TextUtils.isEmpty(itemValue)){
+                itemInt = 0;
+            }else{
+                itemInt = Integer.parseInt(itemValue);
+            }
 
-            //remaining rent
-            remainingRentValue.setText(rentValue);
+            switch (location){
+                case 0:
+                    result = foodInt - itemInt;
+                    foodResult = String.valueOf(result);
+                    remainingFoodValue.setText(foodResult);
+                    break;
+                case 1:
+                    result = rentInt - itemInt;
+                    rentResult = String.valueOf(result);
+                    remainingRentValue.setText(rentResult);
+                    break;
+                case 2:
+                    result = transInt - itemInt;
+                    transResult = String.valueOf(result);
+                    remainingTransitValue.setText(transResult);
+                    break;
+                    default:
+                        remainingFoodValue.setText(foodResult);
+                        remainingRentValue.setText(rentResult);
+                        remainingTransitValue.setText(transResult);
 
-            //remainingTransit
-            remainingTransitValue.setText(transValue);
-        }
-    }
-
-    public void updateValues(View view){
-        //spinner
-        Spinner category = (Spinner) findViewById(R.id.spinner);
-        Integer location = category.getSelectedItemPosition();
-
-        switch (location){
-            case 1:
-                int n1 = Integer.parseInt(foodValue);
-                int n2 = Integer.parseInt(itemValue);
-
-                int n3 = n1 - n2;
-
-                foodValue = String.valueOf(n3);
-
-                remainingFoodValue.setText(foodValue);
+            }
         }
     }
 }
