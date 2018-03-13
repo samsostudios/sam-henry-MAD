b13f754c10f89381ffc807f5c4772b79f9f7688c
Sam Henry <Sam@Sams-MacBook-Pro.local>
2018-03-13 13:36:16 -0600

    milstone 3 updated picker color

---
+++ Spring-2018/Project 1/Milestone 3/Rooted/Rooted/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  Rooted
+//
+//  Created by Sam Henry on 3/11/18.
+//  Copyright © 2018 Sam Henry. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
---
+++ Spring-2018/Project 1/Milestone 3/Rooted/Rooted/plantInfo.swift
@@ -0,0 +1,14 @@
+//
+//  plantIamges.swift
+//  Rooted
+//
+//  Created by Sam Henry on 3/12/18.
+//  Copyright © 2018 Sam Henry. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class PlantInfo: Object {
+    @objc dynamic var type = ""
+}
